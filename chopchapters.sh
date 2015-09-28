f0d560e09ea342ec50d567420399daffaab9da93
glottotopia <sebastian.nordhoff@glottotopia.de>
2015-09-28 20:27:23 +0200

    auto chopping

--- chopchapters.sh
+++ chopchapters.sh
@@ -1,7 +1,7 @@
 i=0 
 o=$1
 old=1
-for l in `cat cuts.txt`
+for l in `cat main.pgs`
 do 
 new=$(($l+o))
 echo $i $old-$(($new-1))
@@ -9,3 +9,5 @@ pdftk main.pdf cat $old-$(($new-1)) output $i.pdf
 old=$new
 i=$(($i+1))
 done
+echo $i $old-end
+pdftk main.pdf cat $old-end output $i.pdf 
