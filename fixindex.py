190e87fe5e79e4a64b1495593b9775944e536569
Sebastian Nordhoff <sebastian.nordhoff@langsci-press.org>
2015-12-15 11:16:55 +0100

    fixindex.py

---
+++ fixindex.py
@@ -0,0 +1,41 @@
+import re
+
+INITD = {
+  'C':'ĆĈ',
+  'd':'đ'
+  }
+  
+orig = ''
+trans = ''
+
+for k in INITD:
+  s = INITD[k]
+  for c in s:
+    orig+=c
+    trans+=k
+     
+transtable = str.maketrans(orig, trans)
+
+p = re.compile(r"\\indexentry \{(.*)\|hyperpage")
+
+    
+def process(s):
+  print(s)
+  m = p.match(s)
+  print(m)
+  o = m.groups(1)[0]
+  t = o.translate(transtable)
+  if t == o:
+    return s
+  else:
+    return s.replace(o,"%s@%s"%(o,t))
+  
+  
+
+if __name__ == '__main__':
+  fn = 'main.adx'
+  lines = open(fn).readlines()
+  print(len(lines))
+  lines2 = list(map(process, lines))
+  print(lines2)
+  
