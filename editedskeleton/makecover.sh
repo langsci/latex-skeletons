032daf6f9cb4580389cad1370863bb9c15179ea8
Sebastian Nordhoff <sebastian.nordhoff@langsci-press.org>
2016-01-05 11:20:45 +0100

    init

---
+++ editedskeleton/makecover.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+#create png graphics of the title page based on metadata parameters
+
+series=$1
+number=$2
+author=$3
+title=$4
+subtitle=$5
+
+cp localmetadata.tex localmetadata.tex~
+
+echo "\author{$3}">> localmetadata.tex
+echo "\title{$4}">> localmetadata.tex
+echo "\subtitle{$5}">> localmetadata.tex
+echo "\renewcommand{\lsSeries}{$1}">> localmetadata.tex 
+echo "\renewcommand{\lsSeriesNumber}{$2}">> localmetadata.tex  
+
+rm main.pdf
+make pdf
+make cover
+mv localmetadata.tex~ localmetadata.tex
