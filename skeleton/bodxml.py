a6e9f8956678869b1542f35aef67ee15b8bc5afb
Sebastian Nordhoff <sebastian.nordhoff@glottotopia.de>
2017-03-13 12:57:01 +0100

    adapt pricing

--- skeleton/bodxml.py
+++ skeleton/bodxml.py
@@ -97,7 +97,7 @@ metadata['colorpagecount'] = len(metadata['colorpages'])
 metadata['colorpagesstring'] = ','.join([str(x) for x in metadata['colorpages']])  
 metadata['pagecount'] = PdfFileReader(open('bodcontent.pdf','rb')).getNumPages()
 #price is 3 EUR base + 3ct per page + 30ct extra per colorpage, rounded up to multiples of 5
-metadata['europrice'] = "%i,%s" %(((300+metadata['pagecount']*3+metadata['colorpagecount']*30)/500+1)*5,"00")
+metadata['europrice'] = "%i,%s" %(((300+metadata['pagecount']*5+metadata['colorpagecount']*30)/500+1)*5,"00")
 metadata['gbprice'] = metadata['europrice']
 metadata['usdprice'] = metadata['europrice'] 
 metadata['binding'] = 'PB'
@@ -106,7 +106,7 @@ outputSC = template.format(**metadata)
 metadata['isbn'] = metadata['isbns']['hardcover'].replace('-','')
 metadata['isbnhc'] = metadata['isbns']['hardcover'].replace('-','')
 #Hardcover is always 10 EUR more than softcover
-metadata['europrice'] = "%i,%s" %(((1300+metadata['pagecount']*3+metadata['colorpagecount']*30)/500+1)*5,"00")
+metadata['europrice'] = "%i,%s" %(((1300+metadata['pagecount']*5+metadata['colorpagecount']*30)/500+1)*5,"00")
 metadata['gbprice'] = metadata['europrice']
 metadata['usdprice'] = metadata['europrice'] 
 metadata['binding'] = 'HC'
