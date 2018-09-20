8f7d766801ef4f25c39d119696678efcee462e6e
glottotopia <sebastian.nordhoff@glottotopia.de>
2018-09-20 06:41:09 +0200

    provide data directory

--- skeleton/assignproofreaders.py
+++ skeleton/assignproofreaders.py
@@ -1,8 +1,22 @@
+"""
+generate templatic emails for community proofreading
+
+The LangSci ID of a book is given as the first argument of the command line. 
+
+The author name to figure in the subject line is given as the second argument
+
+The list of chapters is found in a file `chapternames` with one entry per line 
+
+The matching of community proofreaders and chapters is made in a file `assignments`.
+This file has one name (withouth spaces) and a list of chapternumbers per line. 
+An example is "John 2 3 4"
+
+The due date is set to 4 weeks from today.
+"""
 import sys
 from datetime import datetime, date, time, timedelta
+import os
 
-
-githubid = sys.argv[1] 
 
 template = """Dear  {name},
 thanks for your offer. The book can be found at
@@ -26,19 +40,24 @@ Best wishes and thanks again for your help
 Sebastian
 """
 
-chapters = ['0']+[l.strip() for l in open("chapternames").readlines()]
-assignments = open("assignments").readlines()
 
-mails = []
+if __name__ == "__main__":
+    githubid = sys.argv[1] 
+    authorname = sys.argv[2] 
 
-for a in assignments:
-	name = a.split()[0]
-	chapternumbers = a.split()[1:]
-	chapterlist = '\n'.join("* %s %s"%(i,chapters[int(i)]) for i in chapternumbers)	
-	mails.append(template.format(name=name,   
-			      chapterlist=chapterlist, 
-			      githubid=githubid,
-			      duedate=(datetime.now() + timedelta(weeks=4)).strftime("%B %d")))
-	
-separator =	'\n'+80*'-'+'\n'
-print separator.join(mails)
+    chapters = ['0']+[l.strip() for l in open("chapternames").readlines()] #add dummy 0 to make chapters start with '1'
+    assignments = open("assignments").readlines() 
+    duedate=(datetime.now() + timedelta(weeks=4)).strftime("%B %d")
+    
+    for l in assignments:
+        #parse the chapternames file
+        name = l.split()[0]
+        chapternumbers = l.split()[1:]
+        #collate all assigned chapters
+        chapterlist = '\n'.join("* %s %s"%(i,chapters[int(i)]) for i in chapternumbers)	
+        #generate mail body
+        mailbody = template.format(**locals()) 
+        #launch mail program
+        thunderbirdoptionstring = """-compose "to=%s,subject='proofreading %s',format=2,body='%s'" """%(name,authorname,mailbody) 
+        os.system("thunderbird %s"%thunderbirdoptionstring)
+            
