cb6c2a8f7a0c98485ddbb732b4f4ab05c00f36da
glottotopia <sebastian.nordhoff@glottotopia.de>
2018-11-20 14:22:50 +0100

    clean up

--- skeleton/amazonid.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-ID=$1
-echo https://www.amazon.de/dp/$ID?tag=wwwlangscipre-21
-echo https://www.amazon.co.uk/dp/$ID?tag=wwwlangscip03-21
-echo https://www.amazon.com/dp/$ID?tag=wwwlangscipre-20
--- skeleton/linkchecker.sh
+++
@@ -1,2 +0,0 @@
-echo "Broken links: "
-for f in `grep -ho "www[^ }]*" chapters/*tex`;  do wget --spider  $f; done 2>&1 >/dev/null | grep -B 3 "404 Not"|grep -- --
